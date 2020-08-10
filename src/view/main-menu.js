cfdd379c957cb2e885fdbcbc4126382195ac21de
Vitalii <vitalii.siziakov@gmail.com>
2020-08-10 18:36:15 +0300

    Настройка инфраструктуры и разбивка на модули.

---
+++ src/view/main-menu.js
@@ -0,0 +1,9 @@
+// Меню
+export const createMainMenuTemplate = () => {
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+    <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+    <a class="trip-tabs__btn" href="#">Stats</a>
+   </nav>`
+  );
+};
